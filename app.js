93d72c884120917264c168ea67252549bc19f860
Damir Mustafin <frenchthebread@gmail.com>
2015-09-07 01:04:18 +0900

    function rename

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var config = require('./config');
-var request = require('request')
+var request = require('request');
+var wallpaper = require('wallpaper');
 var $ = require('cheerio');
 
-request('http://photography.nationalgeographic.com/photography/photo-of-the-day/', parseHtml);
+request('http://photography.nationalgeographic.com/photography/photo-of-the-day/', getImageURL);
 
-function parseHtml(err,resp,rawHtml){
+function getImageURL(err,resp,rawHtml){
 
 	var html = $.load(rawHtml);
 
 	html(".primary_photo img").map(function(i,link){
+
 		var src = $(link).attr('src');
     console.log(src);
 
