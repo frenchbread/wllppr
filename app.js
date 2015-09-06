e3e5c32cc67541d6b175b26f3cfb7a6cca14e416
Damir Mustafin <frenchthebread@gmail.com>
2015-09-07 00:56:44 +0900

    get url from national geographic

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var config = require('./config');
+var request = require('request')
+var $ = require('cheerio');
 
-var recent = require('flickr-recent')({
-  key: config.get("apikey")
-});
+request('http://photography.nationalgeographic.com/photography/photo-of-the-day/', parseHtml);
 
-recent(function (error, result) {
-  console.log(result);
-  // => { "id": "14261747769", "owner": "38040930@N05", "secret": "ed6e9380d7", "server": "2925", "farm": 3, "title": "Desert Camo BumbleBot one off #mikedie #bumblebot #resin #dunny #rwk #transformers #bumblebee #camo  Mikedie.bigcartel.com", "ispublic": 1, "isfriend": 0, "isfamily": 0 }
-})
+function parseHtml(err,resp,rawHtml){
+
+	var html = $.load(rawHtml);
+
+	html(".primary_photo img").map(function(i,link){
+		var src = $(link).attr('src');
+    console.log(src);
+
+	});
+
+}
