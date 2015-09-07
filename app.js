fc482adea0e82ebf590f319535ddb0db4754fd11
Damir Mustafin <frenchthebread@gmail.com>
2015-09-08 00:52:30 +0900

    download wallpaper to folder

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var config = require('./config');
 var request = require('request');
 var wallpaper = require('wallpaper');
 var $ = require('cheerio');
+var exec = require('child_process').exec;
 
 request('http://photography.nationalgeographic.com/photography/photo-of-the-day/', getImageURL);
 
@@ -12,8 +13,24 @@ function getImageURL(err,resp,rawHtml){
 	html(".primary_photo img").map(function(i,link){
 
 		var src = $(link).attr('src');
-    console.log(src);
+
+		var url = "http:" + src;
+
+		var location = __dirname + "/Images/file.jpg ";
+		console.log(location)
+
+		exec("wget -O " + location + " " + url, setWallPaper(location) );
 
 	});
 
+}
+
+function setWallPaper(location){
+	wallpaper.set(location, function(err){
+
+		if (err) console.log(err);
+
+		console.log("Done");
+
+	});
 }
