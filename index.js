e0382d300ea3a1516197c76d0aafe7ecac3ccc16
Damir Mustafin <frenchthebread@gmail.com>
2015-09-10 00:12:36 +0900

    app init

---
+++ index.js
@@ -0,0 +1,11 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+app.on('ready', function() {
+
+  var mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600
+  })
+
+});
