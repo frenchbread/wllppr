60fd515921967917fa6815ee4ce9b0ada9c9e8c7
Damir Mustafin <frenchthebread@gmail.com>
2015-09-09 15:06:32 +0900

    random image from https://unsplash.com/ now will be a gb

--- app.js
+++ app.js
@@ -4,17 +4,32 @@ var wallpaper = require('wallpaper');
 var $ = require('cheerio');
 var exec = require('child_process').exec;
 
-request('http://photography.nationalgeographic.com/photography/photo-of-the-day/', function(err,resp,rawHtml){
+request('https://unsplash.com/', function(err,resp,rawHtml){
 
 	var html = $.load(rawHtml);
 
-	html(".primary_photo img").map(function(i,link){
+	var photos = [];
+
+	html(".photo img").map(function(i,link){
 
 		var src = $(link).attr('src');
 
-		var url = "http:" + src;
+		photos.push(src);
 
-		var location = __dirname + "/Images/photo-of-the-day.jpg ";
+
+	});
+
+	console.log(photos.length);
+
+		var photoID = Math.floor(Math.random() * 19) + 0
+
+		console.log("photoID: " + photoID);
+
+		var url = photos[photoID];
+
+		var location = __dirname + "/Images/photo-of-the-day_" + photoID + ".jpg";
+
+		console.log("Staring photo download from " + url + " ...");
 
 		exec("wget -O " + location + " " + url, function (error, stdout, stderr) {
 
@@ -23,16 +38,18 @@ request('http://photography.nationalgeographic.com/photography/photo-of-the-day/
 		  console.log('stdout: ' + stdout);
 		  console.log('stderr: ' + stderr);
 
-			wallpaper.set("./Images/photo-of-the-day.jpg", function(err) {
+			console.log("Downloaded!");
+
+			console.log("Setting photo as desktop background!");
+
+			wallpaper.set("./Images/photo-of-the-day_" + photoID + ".jpg", function(err) {
 
 				if (err) throw err;
 
-				console.log("Background image is set!");
+				console.log("Background photo is set!");
 
 			});
 
 		});
-
-	});
 
 });
